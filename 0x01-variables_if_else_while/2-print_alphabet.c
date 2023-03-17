9ef9815cc588e2f5103fc71d1cf25f75375c2788
wafaasayed19 <wafaa.sayed1122@gmail.com>
2023-03-17 12:41:14 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {       char i;
 
-	for(i = 'a'; i <= 'z' ; i++)
+	for (i = 'a'; i <= 'z' ; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
