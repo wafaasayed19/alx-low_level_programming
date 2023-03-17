40734f1da91230edeeb58f41109867b7d4150850
wafaasayed19 <wafaa.sayed1122@gmail.com>
2023-03-17 15:56:49 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,6 +23,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0", n, m);
 	else if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
-	printf("/n");
+	printf("\n");
 	return (0);
 }
