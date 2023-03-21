5b41faa6dcaab71e9df328196a652c7e0bf07ad8
wafaasayed19 <wafaa.sayed1122@gmail.com>
2023-03-21 03:57:14 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
+/**
  * main - main function
  * _putchar: print the alphabets putchar
  * Return: always 0
  */
 
-int main()
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
