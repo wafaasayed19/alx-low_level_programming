5c3f41fb24607643ceab5a54139b3fcbada9b8cd
wafaasayed19 <wafaa.sayed1122@gmail.com>
2023-04-13 19:06:23 -0700

    commit

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,12 +7,12 @@
 *
 * Return: a pointer to the allocated memory
 */
-void *malloc_checked(unsigned )
+void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
-	return (ptr)
+	return (ptr);
 }
